6682222a716d8f4c12d0e4558db3b7183d3a0029
ERRECabrera <raul@mytwinplace.com>
2017-05-02 17:09:16 +0100

    final gestures app

--- gestures/www/js/gestures.js
+++ gestures/www/js/gestures.js
@@ -24,16 +24,49 @@ var app={
     hammertime.get('pinch').set({enable:true});
     hammertime.get('rotate').set({enable:true});
 
-    hammertime.on('tap doubletap pan swipe press pinch rotate', function(ev){
-      document.querySelector('#info').innerHTML=ev.type+'!';
+    zona.addEventListener('webkitAnimationEnd', function(e){
+      zona.className='';
+    });
+
+    hammertime.on('tap', function(ev){
+      zona.className='tap';
+    });
+
+    hammertime.on('doubletap', function(ev){
+      zona.className='doubletap';
+    });
+
+    hammertime.on('press', function(ev){
+      zona.className='press';
+    });
+
+    hammertime.on('swipe',function(ev){
+      var clase=undefined;
+      direccion=ev.direction;
+
+      if (direccion==4) clase="swipe-derecha";
+      if (direccion==2) clase="swipe-izquierda";
+
+      zona.className=clase;
+    });
+
+    hammertime.on('rotate', function(ev){
+      var umbral=25;
+      if (ev.distance > umbral) zona.className='rotate';
     });
   },
 
   ponloClaro: function(){
+    document.body.addEventListener('webkitAnimationEnd', function(e){
+      document.body.style.backgroundColor = "#8de1bd";
+    });
     document.body.className = 'claro';
   },
 
   ponloOscuro: function(){
+    document.body.addEventListener('webkitAnimationEnd', function(e){
+      document.body.style.backgroundColor = "#577a76";
+    });
     document.body.className = 'oscuro';
   },
 
@@ -41,7 +74,6 @@ var app={
 
 if ('addEventListener' in document) {
   document.addEventListener('DOMContentLoaded',function(){
-
     app.inicio();
   },false);
 }
